5cd7ef5b821cc7a7666d3b90205e850dfdd130b8
Formaou <formao.indes@gmail.com>
2024-02-17 12:15:11 +0300

    ОП (1-2 лаба)

---
+++ 1 курс/2 семестр/Практика/Основы программирования/Лаба 1/area.py
@@ -0,0 +1,7 @@
+import math
+r=5
+area=math.pi*r**2
+print("Площадь круга равна ",area)
+r=5
+circumference=math.pi*r*2
+print("Длина окружности равна ",circumference)
---
+++ 1 курс/2 семестр/Практика/Основы программирования/Лаба 2/27.py
@@ -0,0 +1,8 @@
+i=27
+h=0x1b
+b=0b11011
+o=0o33
+
+print(h-i)
+print(b-i)
+print(o-i)
